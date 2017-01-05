9f83f89b65607d0b54683f27f741f31a544ae5a9
Tamas Tompa <tamas.tompa@codecool.com>
2017-01-05 11:22:44 +0100

    game state

--- player.rb
+++ player.rb
@@ -4,10 +4,11 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
+  	print_r( game_state )
     1000
   end
 
   def showdown(game_state)
-
+  	print_r( game_state )
   end
 end
