215d9d045d706009239890247176b30e64afbbfa
Tamas Tompa <tamas.tompa@codecool.com>
2017-01-05 11:41:28 +0100

    player in action

--- player.rb
+++ player.rb
@@ -1,14 +1,20 @@
 
+require 'json'
+
 class Player
 
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-  	print_r( game_state )
+  	parsed = JSON.parse(game_state)
+  	in_action = parsed["in_action"]
+  	$stderr.puts( "in_action: " + in_action.to_s )
+  	player_in_action = parsed["players"].select{ |list| list["id"] == in_action }
+  	$stderr.puts( "player_in_action: " + player_in_action.to_s )
     1000
   end
 
   def showdown(game_state)
-  	print_r( game_state )
+  	
   end
 end
---
+++ test.rb
@@ -0,0 +1,42 @@
+require "./player"
+
+# Testing
+
+test_json = <<TEST_JSON_STRING
+{
+  "players":[
+    {
+      "name":"Player 1",
+      "stack":1000,
+      "status":"active",
+      "bet":0,
+      "hole_cards":[],
+      "version":"Version name 1",
+      "id":0
+    },
+    {
+      "name":"Player 2",
+      "stack":1000,
+      "status":"active",
+      "bet":0,
+      "hole_cards":[],
+      "version":"Version name 2",
+      "id":1
+    }
+  ],
+  "tournament_id":"550d1d68cd7bd10003000003",
+  "game_id":"550da1cb2d909006e90004b1",
+  "round":0,
+  "bet_index":0,
+  "small_blind":10,
+  "orbits":0,
+  "dealer":0,
+  "community_cards":[],
+  "current_buy_in":0,
+  "in_action":1,
+  "pot":0
+}
+TEST_JSON_STRING
+
+p = Player.new
+p.bet_request( test_json )
