43fec802f1b542b1103b68f92a8c8d821676b41d
Tamas Tompa <tamas.tompa@codecool.com>
2017-01-05 11:46:54 +0100

    player in action

--- player.rb
+++ player.rb
@@ -6,10 +6,9 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-  	parsed = JSON.parse(game_state)
-  	in_action = parsed["in_action"]
+  	in_action = game_state["in_action"]
   	$stderr.puts( "in_action: " + in_action.to_s )
-  	player_in_action = parsed["players"].select{ |list| list["id"] == in_action }
+  	player_in_action = game_state["players"].select{ |list| list["id"] == in_action }
   	$stderr.puts( "player_in_action: " + player_in_action.to_s )
     1000
   end
